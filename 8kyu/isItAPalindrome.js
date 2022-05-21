6d1a50d5995376b6227b2c9a427d958ee6cb4a05
CambodiaJohn <cambodiajohn@protonmail.com>
2022-05-21 17:39:53 +0700

    Refactored is it a palindrome

--- 8kyu/isItAPalindrome.js
+++ 8kyu/isItAPalindrome.js
@@ -1,10 +1,15 @@
 // Write a function that checks if a given string (case insensitive) is a palindrome.
 
-function isPalindrome(x) {
-    let reversed = x.split("").reverse().join("")
-    if(x.toLowerCase() === reversed.toLowerCase()){
-      return true
-    } else {
-      return false
-    }
+// function isPalindrome(x) {
+//     let reversed = x.split("").reverse().join("")
+//     if(x.toLowerCase() === reversed.toLowerCase()){
+//       return true
+//     } else {
+//       return false
+//     }
+//   }
+
+
+const isPalindrome = (x) => {
+    return x.split("").reverse().join("").toLowerCase() === x.toLowerCase() ? true : false
   }
