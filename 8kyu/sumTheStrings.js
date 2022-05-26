eecb304a3a12747ed252d0fa20526fbfafc64e18
CambodiaJohn <cambodiajohn@protonmail.com>
2022-05-26 13:19:45 +0700

    refactored sum the strings code challenge

--- 8kyu/sumTheStrings.js
+++ 8kyu/sumTheStrings.js
@@ -13,7 +13,11 @@
 
 // Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
 
+// function sumStr(a,b) {
+//     sum = Number(a) + Number(b)
+//     return sum.toString()
+//   } 
+
 function sumStr(a,b) {
-    sum = Number(a) + Number(b)
-    return sum.toString()
-  } 
+    return String(Number(a)+Number(b));
+  }
