c9c26960821427c6e3f694880ffd7cf2848ee2bf
CambodiaJohn <cambodiajohn@protonmail.com>
2022-06-14 17:26:07 +0700

    Refactored do you speak english code challenge

--- 8kyu/doYouSpeakEnglish.js
+++ 8kyu/doYouSpeakEnglish.js
@@ -7,7 +7,9 @@
 // Return value as boolean values, true for the string to contains "English", false for it does not.
 
 
-function spEng(sentence){
-    let sentenceLower = sentence.toLowerCase();
-    return sentenceLower.includes('english');
-  }
+// function spEng(sentence){
+//     let sentenceLower = sentence.toLowerCase();
+//     return sentenceLower.includes('english');
+//   }
+
+var spEng = (sentence) => sentence.toLowerCase().includes('english');
