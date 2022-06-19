11b9686280e14ed4e22b75d7cd2d2b03426a2c68
CambodiaJohn <cambodiajohn@protonmail.com>
2022-06-19 17:57:45 +0700

    Refactored drink about

--- 8kyu/drinkAbout.js
+++ 8kyu/drinkAbout.js
@@ -18,15 +18,20 @@
 // 20 --> "drink beer"
 // 30 --> "drink whisky"
 
-function peopleWithAgeDrink(age){
-    if(age < 14)
-      drink = 'toddy';
-    else if(age < 18)
-      drink = 'coke'
-    else if(age < 21)
-      drink = 'beer';
-    else if(age => 21)
-      drink = 'whisky';
+// function peopleWithAgeDrink(age){
+//     if(age < 14)
+//       drink = 'toddy';
+//     else if(age < 18)
+//       drink = 'coke'
+//     else if(age < 21)
+//       drink = 'beer';
+//     else if(age => 21)
+//       drink = 'whisky';
   
-    return 'drink ' + drink;
-  }
+//     return 'drink ' + drink;
+//   }
+
+const peopleWithAgeDrink = (age) =>
+  age < 14 ? "drink toddy" :
+  age < 18 ? "drink coke" :
+  age < 21 ? "drink beer" : "drink whisky"
