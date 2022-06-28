034002f358d04c013aecfda1f9f14e4fde7a5e2b
CambodiaJohn <cambodiajohn@protonmail.com>
2022-06-28 14:51:32 +0700

    Refactored sum Of Positives code challenge

--- 8kyu/sumOfPositive.js
+++ 8kyu/sumOfPositive.js
@@ -4,12 +4,17 @@
 
 // Note: if there is nothing to sum, the sum is default to 0.
 
-function positiveSum(arr) {
-    var total = 0;    
-    for (i = 0; i < arr.length; i++) {    
-      if (arr[i] > 0) {                   
-        total += arr[i];                  
-      }
-    }
-    return total;                        
-  }
+// function positiveSum(arr) {
+//     var total = 0;    
+//     for (i = 0; i < arr.length; i++) {    
+//       if (arr[i] > 0) {                   
+//         total += arr[i];                  
+//       }
+//     }
+//     return total;                        
+//   }
+
+
+  function positiveSum(arr) {
+    return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+ }
